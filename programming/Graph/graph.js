0e4da3fc018f9406941fd8a8a4cd2b91940abb01
dayanandraut <dayanandraut2015@gmail.com>
2020-01-25 21:00:20 +0530

    info about graph

---
+++ programming/Graph/graph.js
@@ -0,0 +1,13 @@
+// Graph is used for representing real life relationships
+// Graph has node called vertex
+// vertices are connected with each other using edge
+/*
+TYPES OF GRAPH
+    1. Directed and Undirected
+    2. Weighted and Unweighted (Weight can be on node or edge)
+    3. Acyclic and Cyclic 
+
+DAG : Directed Acyclic Graph
+
+Note: Linked list is a type of tree, and tree is a type of graph.
+*/
